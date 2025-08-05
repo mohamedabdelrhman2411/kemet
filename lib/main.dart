d0e6e60f27b5abf59c98b7f08b2f7ba90e5c8631
mohamed abdelrhman <mohamed.abdelrhman21@gmail.com>
2025-08-05 13:38:02 +0300

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const Kemet());
+}
+
+class Kemet extends StatelessWidget {
+  const Kemet({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
